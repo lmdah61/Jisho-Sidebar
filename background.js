e03e57b48f773869379cb28fa3907645e62ebb62
H <123673560+lmdah61@users.noreply.github.com>
2023-12-08 02:21:57 -0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,19 @@
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'openSidePanel',
+    title: 'Open side panel',
+    contexts: ['all']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'openSidePanel') {
+    // This will open the panel in all the pages on the current window.
+    chrome.sidePanel.open({ windowId: tab.windowId });
+  }
+});
---
+++ panel.js
@@ -0,0 +1,5 @@
+// panel.js
+
+// This file doesn't need to contain any code for this project, as all the functionality is already handled by the background.js file and the iframe in panel.html. The iframe directly loads the jisho.org website, and the background.js file handles the creation of the popup window when the browser action is clicked.
+
+// If you wanted to add additional functionality to the panel, such as interacting with the jisho.org website or manipulating the DOM of the panel.html file, you would add that code here.
